53a206d0feddc8c2a118ed7facbbd9534aa3c8a6
benlamb95 <benl1995@hotmail.co.uk>
2022-01-05 12:34:02 +0000

    Script for modal created

---
+++ assets/script/script.js
@@ -0,0 +1,23 @@
+
+
+/* -- Modal Script -- */
+
+var modal = document.getElementById("playModal")
+
+var button = document.getElementById("how-to-play")
+
+var span = document.getElementsByClassName("close")[0]
+
+button.onclick = function () {
+    modal.style.display = "block"
+}
+
+span.onclick = function () {
+    modal.style.display = "none"
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
