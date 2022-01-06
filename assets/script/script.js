00b640bcfd27882a0c75587819a39696830782e4
benlamb95 <benl1995@hotmail.co.uk>
2022-01-06 10:48:53 +0000

    Template for functions added to help visualise whats needed for game to work

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,12 @@
 /* -- Variables -- */
 
 /* -- Audio -- */
+/* Help from  https://www.youtube.com/watch?v=wffK2OIt8u0 */
 const backgroundAudio = new Audio('assets/audio/background.mp3')
 backgroundAudio.loop = true;
 backgroundAudio.volume = 0.3;
 var playPauseIcon = document.getElementById('play-pause')
+var count = 0;
 
 const flipAudio = new Audio('assets/audio/acardflip.wav')
 flipAudio.volume = 0.5;
@@ -12,6 +14,38 @@ const matchAudio = new Audio('assets/audio/match.wav')
 matchAudio.volume = 0.5;
 const winnerAudio = new Audio('assets/audio/winner.mp3')
 winnerAudio = 0.6;
+
+/* -- Functions for game template -- */
+
+function startGame(){
+    
+}
+
+function flipCard(){
+    
+}
+
+
+function shuffleCards(){
+    
+}
+
+function checkMatch(){
+    
+}
+
+function matchedCards(){
+
+}
+
+function unmatchedCards(){
+    
+}
+
+function winner(){
+    
+}
+
 
 
 /* -- Modal Script -- */
