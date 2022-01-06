614ff66074496efc113cf6fd59cf5190039af3c8
benlamb95 <benl1995@hotmail.co.uk>
2022-01-06 10:35:38 +0000

    Add audio files and variables to script file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,17 @@
+/* -- Variables -- */
+
+/* -- Audio -- */
+const backgroundAudio = new Audio('assets/audio/background.mp3')
+backgroundAudio.loop = true;
+backgroundAudio.volume = 0.3;
+var playPauseIcon = document.getElementById('play-pause')
+
+const flipAudio = new Audio('assets/audio/acardflip.wav')
+flipAudio.volume = 0.5;
+const matchAudio = new Audio('assets/audio/match.wav')
+matchAudio.volume = 0.5;
+const winnerAudio = new Audio('assets/audio/winner.mp3')
+winnerAudio = 0.6;
 
 
 /* -- Modal Script -- */
